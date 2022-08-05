0681fcf94312a9e3036cc7308d5628568e0b6733
Arash Goodarzi <arash.goodarzi@divar.ir>
2022-08-05 17:54:24 +0430

    Initial Commit

---
+++ Shared/Annoatation_InjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Annoatation_InjectionApp.swift
+//  Shared
+//
+//  Created by Arash on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Annoatation_InjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
